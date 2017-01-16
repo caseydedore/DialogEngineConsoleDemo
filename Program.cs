0f8b2d41feba377c9ded7887ee4a4ff0c2ce29a7
caseydedore <caseydedore@gmail.com>
2017-01-15 21:30:41 -0800

    Created DialogEngine demo app

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using DataReader;
+using DialogEngine.Model;
+using System;
+using System.IO;
+
+namespace DialogEngineConsoleDemo
+{
+    public class Program
+    {
+        private FileReader fileReader = new FileReader();
+        private XmlDataReader xmlReader = new XmlDataReader();
+
+
+        public static void Main(string[] args)
+        {
+            var program = new Program();
+
+            var data = program.LoadData(); 
+        }
+
+        private ConversationData LoadData()
+        {
+            var fileData = fileReader.Load(Path.GetFullPath("Data"), "conversation.xml");
+            var xmlData = xmlReader.Deserialize<ConversationData>(fileData);
+
+            return xmlData;
+        }
+    }
+}
